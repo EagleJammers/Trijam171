58bb702910006dc956c7f3d9dcf44eb20e6fc319
Clockknight <34818025+Clockknight@users.noreply.github.com>
2022-05-29 14:56:08 -0700

    Update Board manager start call

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    //Tile[][] Board;
-    //Player p;
-    int dimension = 0;
-    float minePercent = 20f;
+    private Tile[][] Board;
+    private Player p;
+    [serializefield]
+    private Tile tObj;
+
+    private int dimension = 0;
+    private float minePercent = .20f;
+    private float minePresent = 0f;
+
+    private float percentbytile = (float) 1 / (dimension * dimension);
+    private int tilepermine = (int) dimension * dimension * minePercent;
 
     // Start is called before the first frame update
     void Start()
     {
-      //Find Player object
-      //Find Mine objects and insert them into Board array
+      //Find Player object by Tag
+
+      int tilesunmined = 0;
+      //Instantiate Mine objects and insert them into Board array
+      for(int x;x < dimension;x++)
+      {
+        for(int y;y<dimensions;y++)
+        {
+          //Tile t = Instantiate new Tile
+          //Board[x][y] = t
+          //Move that tile to some place on the UI based on screen size vs how many we have
+
+          //Set Mine flag to true or false based on RNG
+            //if set to true
+              //minepresent += percentbytile
+              //tilesunmined = 0;
+
+            //if set to false
+          //if tiles unmined >= tiles permine, set tilesunmined = 0, set mine flag to true
+          //else increase tilesunmined by 1
+
+        }
+      }
     }
 
     // Update is called once per frame
